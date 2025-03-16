adc5cdd601cef80bd3b0045328c9af17d4a31870
Jorge San Martin <jsanmart666@gmail.com>
2025-03-15 21:41:20 -0300

    Se agrega implementacion de API

--- PPDApp/planes_admin/urls.py
+++ PPDApp/planes_admin/urls.py
@@ -1,8 +1,18 @@
-from django.urls import path
 from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PlanViewSet, MedidaViewSet, OrganismoViewSet
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+router = DefaultRouter()
+router.register(r'plans', PlanViewSet)
+router.register(r'medidas', MedidaViewSet)
+router.register(r'organismos', OrganismoViewSet)
 
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('agregar_plan/', views.agregar_plan),
     path('ver_planes/', views.lista_plan, name='lista_plan'),
     path('detalle_plan/<int:pk>', views.detalle_plan, name='detalle_plan'),
