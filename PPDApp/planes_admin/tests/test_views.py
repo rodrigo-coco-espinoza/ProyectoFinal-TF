e82fef297645f948386f67e4f2cca9ad1a340363
Jorge San Martin <jsanmart666@gmail.com>
2025-04-20 23:26:21 -0400

    Se creo prueba con validacion de rol para la creacion de plan

--- PPDApp/planes_admin/tests/test_views.py
+++ PPDApp/planes_admin/tests/test_views.py
@@ -9,12 +9,9 @@ from planes_admin.models import *
 from user.models import Usuario
 from django.contrib.auth.models import Group, Permission
 
-
-
 @pytest.fixture
-def api_client():
+def api_client_planificador():
     return APIClient()
-
 
 @pytest.fixture
 def plan_data():
@@ -62,9 +59,29 @@ def crear_roles():
 
             grupo.save()
 
+@pytest.fixture
+def crear_usuario_admin(db):
+    usuario = Usuario.objects.create_user(email="admin@ppda.cl", password=PASSWORD)
+    grupo, creado = Group.objects.get_or_create(name="Administrador")
+
+    usuario.groups.add(grupo)
+    usuario.save()
+    return usuario
 
 @pytest.fixture
-def crear_usuario(db):
+def jwt_token_admin(crear_usuario_admin, crear_roles):
+    client = APIClient()
+    response = client.post("/api/token/", {"email": "admin@ppda.cl", "password": PASSWORD})
+    return response.data["access"]
+
+@pytest.fixture
+def api_client_admin(jwt_token_admin):
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION=f'Bearer {jwt_token_admin}')
+    return client
+
+@pytest.fixture
+def crear_usuario_planificador(db):
     usuario = Usuario.objects.create_user(email=USERNAME, password=PASSWORD)
     grupo, creado = Group.objects.get_or_create(name="Planificador")
 
@@ -73,62 +90,67 @@ def crear_usuario(db):
     return usuario
 
 @pytest.fixture
-def jwt_token(crear_usuario, crear_roles):
+def jwt_token_planificador(crear_usuario_planificador, crear_roles):
     client = APIClient()
     response = client.post("/api/token/", {"email": USERNAME, "password": PASSWORD})
     return response.data["access"]
 
 @pytest.fixture
-def api_client(jwt_token):
+def api_client_planificador(jwt_token_planificador):
     client = APIClient()
-    client.credentials(HTTP_AUTHORIZATION=f'Bearer {jwt_token}')
+    client.credentials(HTTP_AUTHORIZATION=f'Bearer {jwt_token_planificador}')
     return client
 
 
-def test_obtener_permisos(api_client, jwt_token):
+def test_obtener_permisos(api_client_planificador, jwt_token_planificador):
     url = reverse("permisos")
-    response = api_client.get(url)
+    response = api_client_planificador.get(url)
 
     assert response.status_code == 200, f"❌ Error 401, respuesta: {response.data}"
     print("🔹 Permisos del usuario:", response.data["permisos"])
 
 @pytest.mark.django_db
 class TestPlanViewSet:
-    def test_list_planes(self, api_client, plan):
+    def test_list_planes(self, api_client_planificador, plan):
         url = reverse("plan-list")
-        response = api_client.get(url)
+        response = api_client_planificador.get(url)
         print("🔹 Permisos requeridos por la API:", response)
 
         assert response.status_code == status.HTTP_200_OK
         #assert len(response.data) == 1
 
-    def test_create_plan(self, api_client, plan_data):
+    def test_create_plan(self, api_client_planificador, plan_data):
         url = reverse("plan-list")
-        response = api_client.post(url, plan_data, format="json")
+        response = api_client_planificador.post(url, plan_data, format="json")
         assert response.status_code == status.HTTP_201_CREATED
         assert Plan.objects.count() == 1
 
-    def test_retrieve_plan(self, api_client, plan_data):
+    def test_retrieve_plan(self, api_client_planificador, plan_data):
         plan = Plan.objects.create(**plan_data)
         url = reverse("plan-detail", args=[plan.id])
-        response = api_client.get(url)
+        response = api_client_planificador.get(url)
         assert response.status_code == status.HTTP_200_OK
         assert response.data["nombre"] == plan_data["nombre"]
 
-    def test_update_plan(self, api_client, plan_data):
+    def test_update_plan(self, api_client_planificador, plan_data):
         plan = Plan.objects.create(**plan_data)
         url = reverse("plan-detail", args=[plan.id])
         updated_data = plan_data.copy()
         updated_data["nombre"] = "Gabriel José"
-        response = api_client.put(url, updated_data, format="json")
+        response = api_client_planificador.put(url, updated_data, format="json")
         assert response.status_code == status.HTTP_200_OK
         assert response.data["nombre"] == "Gabriel José"
 
-    def test_delete_plan(self, api_client, plan_data):
+    def test_delete_plan(self, api_client_planificador, plan_data):
         plan = Plan.objects.create(**plan_data)
         url = reverse("plan-detail", args=[plan.id])
-        response = api_client.delete(url)
+        response = api_client_planificador.delete(url)
         assert response.status_code == status.HTTP_204_NO_CONTENT
         assert Plan.objects.count() == 0
 
-
+    def test_create_plan_admin(self, api_client_admin, plan_data):
+        # Se valida que el usuario admin no puede usar la API que crea planes
+        url = reverse("plan-list")
+        response = api_client_admin.post(url, plan_data, format="json")
+        assert response.status_code == 403
+        assert Plan.objects.count() == 0
